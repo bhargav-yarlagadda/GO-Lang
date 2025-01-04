db36986a00a4ebecfbcf755c9d9c0fa743a6423d
bhargav-yarlagadda <bhargavyarlagadda>
2025-01-04 19:01:22 +0530

    chore

---
+++ introduction.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World!")
+}
+// fmt is the module for I/O
+// program execution starts at amin
+// every go file is a package 
+// main is a special package that specifics that a file contains the main program
